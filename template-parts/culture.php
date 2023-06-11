3ed98d3e963f14bbb270c638e98c779141122e0f
dfox <dfox@foxartbox.com>
2023-06-11 08:42:18 +0300

    Culture. Part 1

--- template-parts/culture.php
+++ template-parts/culture.php
@@ -1,3 +1,28 @@
 <section id="culture" class="culture">
-	<h2>Culture</h2>
+	<div class="culture__wrapper">
+		<div class="culture__decoration culture-decoration" aria-hidden="true">
+			<div class="culture-decoration__label">
+				<span class="culture-decoration__label-inner">Company</span>
+			</div>
+
+			<div class="culture-decoration__label">
+				<span class="culture-decoration__label-inner">Culture</span>
+			</div>
+
+			<span class="culture-decoration__number">002</span>
+		</div>
+
+		<div class="culture__content culture-content ">
+			<header class="culture__header culture-header">
+				<span class="culture-header__subtitle">Current Openings</span>
+				<h2 class="culture-header__title">Open Roles</h2>
+			</header>
+
+			<strong class="culture-content__featured">Lorem ipsum dolor sit amet, consectetur adipisp quis nostrud exercitation.</strong>
+
+			<p class="culture-content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa.</p>
+
+			<a href="#" class="culture-content__button">View All Open Roles</a>
+		</div>
+	</div>
 </section>
--- template-parts/openings.php
+++ template-parts/openings.php
@@ -4,13 +4,14 @@
 			<div class="openings-decoration__label">
 				<span class="openings-decoration__label-inner">Open</span>
 			</div>
+
 			<div class="openings-decoration__label">
 				<span class="openings-decoration__label-inner">Roles</span>
 			</div>
-			<span class="openings-decoration__number">
-					001
-			</span>
+
+			<span class="openings-decoration__number">001</span>
 		</div>
+
 		<div class="openings__content openings-content ">
 			<header class="openings__header openings-header">
 				<span class="openings-header__subtitle">Current Openings</span>
@@ -22,21 +23,23 @@
 			<p class="openings-content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa.</p>
 			<a href="#" class="openings-content__button">View All Open Roles</a>
 		</div>
+
 		<div class="openings__listings openings-listings">
 			<div class="openings__listings-header openings-listings-header">
 				<span class="openings-listings-header__title openings-listings-header__title--position">Position</span>
 				<span class="openings-listings-header__title openings-listings-header__title--location">Location</span>
 				<span class="openings-listings-header__title openings-listings-header__title--duration">Duration</span>
 			</div>
+
 			<ul class="openings__list openings-list">
-                <?php for($i = 0; $i < 4; $i++) : ?>
+				<?php for($i = 0; $i < 4; $i++) : ?>
 					<li class="openings-list__item">
-	                    <a href="#" class="openings-list__link">
-	                        <span class="openings-list__position">Operations Manager</span>
-	                        <span class="openings-list__location">Katy, Tx</span>
-	                        <span class="openings-list__duration">Full Time</span>
-	                    </a>
-	                </li>
+						<a href="#" class="openings-list__link">
+							<span class="openings-list__position">Operations Manager</span>
+							<span class="openings-list__location">Katy, Tx</span>
+							<span class="openings-list__duration">Full Time</span>
+						</a>
+					</li>
 				<?php endfor; ?>
 			</ul>
 		</div>
